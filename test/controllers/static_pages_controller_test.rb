b4aed8ffb4dabfca417878936f478511f53a292c
Matthew Buttler <matthew.buttler@gmail.com>
2017-11-21 21:21:46 -0500

    🥉

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,6 +6,12 @@ def setup
   @base_title = "Four Loko Paradise"
 end
 
+  test "should get root" do
+    get root_url
+    assert_response :success
+    assert_select "title", "Home | #{@base_title}"
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
@@ -22,5 +28,11 @@ end
     get static_pages_about_url
     assert_response :success
     assert_select "title", "About | #{@base_title}"
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 end
